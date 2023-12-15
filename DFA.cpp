00ecac7141fc8d5dfa7a0c8100197fb43f80261f
denisleuta <146765040+denisleuta@users.noreply.github.com>
2023-12-15 21:51:25 +0300

    Add files via upload

---
+++ DFA.cpp
@@ -0,0 +1,44 @@
+﻿
+#define DFA_CPP  
+#include "DFA.h"
+
+template <typename State, typename Symbol>
+DFA<State, Symbol>::DFA(const std::unordered_set<State>& states,
+    const std::unordered_set<Symbol>& alphabet,
+    const State& initial,
+    const State& output,
+    const std::unordered_map<State, std::unordered_map<Symbol, State>>& transitionTable,
+    size_t alphabetSize)
+    : initialState(initial), outputState(output), transitions(transitionTable) {
+
+    if (states.find(initial) == states.end() ||
+        states.find(output) == states.end()) {
+        throw std::invalid_argument("Invalid DFA parameters");
+    }
+
+    if (alphabet.size() != alphabetSize) {
+        throw std::invalid_argument("Invalid alphabet size for DFA");
+    }
+}
+
+template <typename State, typename Symbol>
+bool DFA<State, Symbol>::checkString(const std::vector<Symbol>& input) {
+    State currentState = initialState;
+    size_t currentIndex = 0;
+
+    for (const Symbol& symbol : input) {
+        if (transitions[currentState].find(symbol) == transitions[currentState].end()) {
+            return false;
+        }
+
+        currentState = transitions[currentState][symbol];
+
+        if (currentState == outputState && currentIndex == input.size() - 1) {
+            return true;
+        }
+
+        currentIndex++;
+    }
+
+    return false;
+}
---
+++ DFA.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <iostream>
+#include <unordered_set>
+#include <unordered_map>
+#include <string>
+#include <vector>
+
+template <typename State, typename Symbol>
+class DFA {
+private:
+    std::unordered_map<State, std::unordered_map<Symbol, State>> transitions;
+    State initialState;
+    State outputState;
+
+public:
+    DFA(const std::unordered_set<State>& states,
+        const std::unordered_set<Symbol>& alphabet,
+        const State& initial,
+        const State& output,
+        const std::unordered_map<State, std::unordered_map<Symbol, State>>& transitionTable,
+        size_t alphabetSize);
+
+    bool checkString(const std::vector<Symbol>& input);
+};
+#ifndef DFA_CPP
+#include "DFA.cpp"
+#endif
+
